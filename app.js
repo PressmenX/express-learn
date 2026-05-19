94af9eecb39cd50d5043064a2b7f8b9bb4618e60
Daffa Alfin Alfikri <affah2212@gmail.com>
2026-05-19 23:04:46 +0700

    practice : req and res object

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 const products = [
-  {id : 1, name : "Milk"},
-  {id : 2, name : "Coffe"},
+  { id: 1, name: "Milk" },
+  { id: 2, name: "Coffe" },
 ];
 
 app.use(express.json());
@@ -13,42 +13,53 @@ app.get("/", (req, res) => {
 });
 
 app.get("/products", (req, res) => {
-  res.json({ products });
+  const { category, limit } = req.query;
+  res.json({
+    category,
+    limit : Number(limit),
+    data: products,
+  });
 });
 
 app.get("/products/:id", (req, res) => {
   const id = Number(req.params.id);
-  const index = products.findIndex(p => p.id === id)
+  const index = products.findIndex((p) => p.id === id);
   res.json({ data: products[index] });
 });
 
 app.post("/products", (req, res) => {
-  const data = req.body;
+  const { id, name } = req.body;
+
+  if (!id || !name) {
+    return res.status(400).json({ message: "Data tidak Valid" });
+  }
+
+  const data = { id, name };
   products.push(data);
-  res.json({ message: "Data berhasil dikirim!", data });
+  res.status(201).json({ message: "Data berhasil dibuat!", created : data });
 });
 
-app.put("/products/:id", (req, res)=> {
-  const id= Number(req.params.id)
-  const data = req.body
-  const index = products.findIndex(p => p.id === id)
+app.put("/products/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const data = req.body;
+  const index = products.findIndex((p) => p.id === id);
   if (index !== -1) {
-    products[index] = {id , ...data}
-    res.json({data : products[index]})
-  }else {
-     res.status(404).json({message : "Product tidak ditemukan"})
+    products[index] = { id, ...data };
+    res.json({ data: products[index] });
+  } else {
+    res.status(404).json({ message: "Product tidak ditemukan" });
   }
-})
+});
 
 app.delete("/products/:id", (req, res) => {
-  const id= Number(req.params.id)
-  const index = products.findIndex(p => p.id === id)
+  const id = Number(req.params.id);
+  const index = products.findIndex((p) => p.id === id);
   if (index !== -1) {
-    products.splice(index, 1)
-    res.json({message : `Data ${id} berhasil Di hapus`})
-  }else {
-    res.status(404).json({message : "Product tidak ditemukan"})
+    products.splice(index, 1);
+    res.json({ message: `Data ${id} berhasil Di hapus` });
+  } else {
+    res.status(404).json({ message: "Product tidak ditemukan" });
   }
-})
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -5,6 +5,28 @@ const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, async () => {
   console.log("Server Berjalan di http://localhost:3000");
 
-  const res = await fetch("http://localhost:3000/products/1")
+  const res = await fetch(
+    "http://localhost:3000/products?category=drinks&limit=5",
+  );
   console.log(await res.json());
+
+
+  const postRes = await fetch("http://localhost:3000/products", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      id: 3,
+      name: "Green Tea",
+    }),
+  });
+  console.log(await postRes.json());
+
+   const postRes2 = await fetch("http://localhost:3000/products", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      id: 4,
+    }),
+  });
+  console.log(await postRes2.json());
 });
